87f396d5f37141a3d610b58885f5b3794102d630
champaincode <edgarlagos.355@hotmail.com>
2023-02-17 00:31:41 -0300

    feat:add view manager schedule from management

---
+++ client/src/pages/views/progress.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import CircularProgress, { CircularProgressProps } from '@mui/material/CircularProgress';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+function CircularProgressWithLabel(props: CircularProgressProps & { value: number }) {
+  return (
+    <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+      <CircularProgress variant="determinate" {...props} />
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: 'absolute',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Typography
+          sx={{ fontFamily: 'Roboto', fontWeight: 700, color: 'black' }}
+          variant="caption"
+          component="div"
+        >{`${Math.round(props.value)}%`}</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+export default function CircularStatic() {
+  const [progress, setProgress] = React.useState(20);
+
+  return (
+    <>
+      {' '}
+      <Box sx={{ marginLeft: '2%' }}>
+        <CircularProgressWithLabel sx={{ color: '#6373F7' }} size={60} value={progress} />
+      </Box>
+    </>
+  );
+}
