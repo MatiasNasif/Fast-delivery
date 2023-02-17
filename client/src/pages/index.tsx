88a44aca4c0605c2b37051ff37b1f1d546b49774
champaincode <edgarlagos.355@hotmail.com>
2023-02-17 00:47:48 -0300

    feat: add view login and register

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -4,7 +4,7 @@ import { Inter } from '@next/font/google';
 import styles from '@/styles/Login.module.css';
 import { Box, TextField, Container, Button, Typography } from '@mui/material';
 import Image from 'next/image';
-import brand from './assets/brand.png';
+import brand from '../../src/assets/brand.png';
 import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
