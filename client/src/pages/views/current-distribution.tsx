e3cd2562afef4a4760d2b16100d5560b35817eba
MatiasNasif <matiasezequielnasif@gmail.com>
2023-02-20 18:06:13 -0300

    refactor: modularize css styles

--- client/src/pages/views/current-distribution.tsx
+++ client/src/pages/views/current-distribution.tsx
@@ -1,4 +1,10 @@
+import styles from '../../styles/CurrentDistribution.module.css';
 import Image from 'next/image';
+import brand from '../../assets/brand.png';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import maps from '../../assets/maps.png';
+import dummyData from '../../dummy-data/package-dummy.json';
 import {
   Box,
   Container,
@@ -8,113 +14,56 @@ import {
   AccordionDetails,
   Typography,
 } from '@mui/material';
-import brand from '../../assets/brand.png';
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import maps from '../../assets/maps.png';
-import dummyData from '../../dummy-data/package-dummy.json';
 
 export default function CurrentDistribution() {
   return (
     <Container maxWidth="xs" disableGutters={true}>
-      <Box
-        component="form"
-        sx={{
-          display: 'flex',
-          justifyContent: 'flex-start',
-          marginBottom: '10px',
-          background: '#FFFFFF',
-          WebkitBoxShadow: '0px 2px 5px -1px rgba(0,0,0,0.75)',
-          MozBoxShadow: ' 0px 2px 5px -1px rgba(0,0,0,0.75)',
-          boxShadow: '0px 2px 5px -1px rgba(0,0,0,0.75)',
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <Image src={brand} alt="brand" width={51} height={32} style={{ marginBottom: '5px' }} />
+      <Box component="form" className={styles.header__container} noValidate autoComplete="off">
+        <Image className={styles.header_container_image} src={brand} alt="brand" />
       </Box>
 
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'flex-start',
-          marginBottom: '10px',
-        }}
-      >
-        <KeyboardArrowLeftIcon width={26} height={26} />
+      <Box className={styles.container_icon_arrow}>
+        <KeyboardArrowLeftIcon className={styles.icon_arrow} />
       </Box>
 
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-        }}
-      >
+      <Box className={styles.container_accordion}>
         <Accordion>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon />}
             aria-controls="panel1a-content"
             id="panel1a-header"
           >
-            <Typography variant="h6" sx={{ fontWeight: 'bold', fontSize: '16px' }}>
+            <Typography variant="h6" className={styles.container_accordion_title}>
               Reparto en curso
             </Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Box>
-              <Image src={maps} alt="maps" width={287} height={212} />
+              <Image src={maps} alt="maps" className={styles.container_accordion_maps} />
             </Box>
             <Box>
-              <Typography
-                sx={{
-                  fontSize: '14px',
-                  color: '#4F4F4F',
-                  fontWeight: 'bold',
-                }}
-              >
-                Destino: <span style={{ fontWeight: 'normal' }}>{dummyData[0].address}</span>
+              <Typography className={styles.container_accordion_subtitle}>
+                Destino:{' '}
+                <span className={styles.container_accordion_subtitle_details}>
+                  {dummyData[0].address}
+                </span>
               </Typography>
-              <Typography
-                sx={{
-                  fontSize: '14px',
-                  color: '#4F4F4F',
-                  fontWeight: 'bold',
-                }}
-              >
-                # del paquete: <span style={{ fontWeight: 'normal' }}>{dummyData[0].id}</span>
+              <Typography className={styles.container_accordion_subtitle}>
+                # del paquete:{' '}
+                <span className={styles.container_accordion_subtitle_details}>
+                  {dummyData[0].id}
+                </span>
               </Typography>
-              <Typography
-                sx={{
-                  fontSize: '14px',
-                  color: '#4F4F4F',
-                  fontWeight: 'bold',
-                }}
-              >
-                Recibe: <span style={{ fontWeight: 'normal' }}>{dummyData[0].receiver}</span>
+              <Typography className={styles.container_accordion_subtitle}>
+                Recibe:{' '}
+                <span className={styles.container_accordion_subtitle_details}>
+                  {dummyData[0].receiver}
+                </span>
               </Typography>
             </Box>
           </AccordionDetails>
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'flex-end',
-              marginRight: '10px',
-              marginBottom: '10px',
-            }}
-          >
-            <Button
-              sx={{
-                fontStyle: 'normal',
-                fontWeight: '500',
-                fontSize: '15px',
-                lineHeight: '26px',
-                letterSpacing: '0.46px',
-                textTransform: 'uppercase',
-                color: '#FFFFFF',
-                borderRaduis: '5px',
-              }}
-              variant="contained"
-            >
+          <Box className={styles.container_button}>
+            <Button className={styles.button} variant="contained">
               Finalizar
             </Button>
           </Box>
