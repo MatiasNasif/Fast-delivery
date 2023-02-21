b1b7745ec113e3b23f70c3be94b6e441ba9de984
champaincode <edgarlagos.355@hotmail.com>
2023-02-21 13:59:54 -0300

    css modularization, refactoring and componentization

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,11 @@
-/* eslint-disable @next/next/no-page-custom-font */
 import Head from 'next/head';
 import { Inter } from '@next/font/google';
-import styles from '@/styles/Login.module.css';
-import { Box, TextField, Container, Button, Typography } from '@mui/material';
+import styles from '../styles/Login.module.css';
+import { Box, TextField, Container, Button, Typography, FormControl } from '@mui/material';
 import Image from 'next/image';
 import brand from '../../src/assets/brand.png';
 import Link from 'next/link';
+import InputPassword from '../commons/InputPassword';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,129 +14,40 @@ export default function Home() {
     <>
       <Head>
         <title>Login</title>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
       </Head>
       <Container maxWidth={'xs'}>
-        <Box
-          component="form"
-          sx={{
-            width: '100%',
-            height: '132px',
-            display: 'flex',
-            justifyContent: 'center',
-          }}
-          noValidate
-          autoComplete="off"
-        ></Box>
-        <Box
-          component="form"
-          sx={{
-            width: '100%',
-            height: '151px',
-            display: 'flex',
-            justifyContent: 'center',
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <Image
-            className={styles.brand}
-            src={brand}
-            alt="Fast Delivery Brand"
-            width={149}
-            height={94}
-          />
+        <Box className={styles.boxspace}></Box>
+        <Box className={styles.boxBrand}>
+          <Image className={styles.brand} src={brand} alt="Fast Delivery Brand" />
         </Box>
-        <Box
-          component="form"
-          sx={{
-            marginBottom: '30px',
-            display: 'flex',
-            justifyContent: 'center',
-            maxWidth: '100%',
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            label="Usuario"
-            InputLabelProps={{ className: styles.textLabelcolor }}
-            variant="standard"
-            sx={{ fontFamily: 'Roboto' }}
-            focused
-            fullWidth
-          />
-        </Box>
-        <Box
-          component="form"
-          sx={{ display: 'flex', justifyContent: 'center', maxWidth: '100%' }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            label="Contraseña"
-            InputLabelProps={{ className: styles.textLabelcolor }}
-            variant="standard"
-            sx={{ fontFamily: 'Roboto' }}
-            focused
-            fullWidth
-          />
-        </Box>
-        <Box
-          component="form"
-          sx={{
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            marginTop: '25px',
-            justifyContent: 'center',
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <Button fullWidth variant="contained">
-            Ingresar
-          </Button>
-        </Box>
-        <Box
-          component="form"
-          sx={{
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            marginTop: '25px',
-            justifyContent: 'center',
-          }}
-          noValidate
-          autoComplete="off"
-        >
+        <FormControl className={styles.form}></FormControl>
+        <TextField
+          label="Usuario"
+          InputLabelProps={{ className: styles.textLabelcolor }}
+          variant="standard"
+          sx={{ fontFamily: 'Roboto' }}
+          focused
+          fullWidth
+        />
+        <InputPassword />{' '}
+        <Button fullWidth variant="contained">
+          Ingresar
+        </Button>
+        <Box className={styles.boxLinks}>
           <Typography
-            sx={{ fontSize: '19px', fontWeight: '300', fontFamily: 'Roboto' }}
+            className={styles.TextLiks}
+            sx={{ fontWeight: '300' }}
             variant="inherit"
             color="primary"
           >
             Recuperar contraseña
           </Typography>
-        </Box>
-        <Box
-          component="form"
-          sx={{
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            marginTop: '25px',
-            justifyContent: 'center',
-          }}
-          noValidate
-          autoComplete="off"
-        >
+
           <Link href="/views/register">
             {' '}
             <Typography
-              sx={{ fontSize: '19px', fontWeight: '700', fontFamily: 'Roboto' }}
+              className={styles.TextLiks}
+              sx={{ fontWeight: '700' }}
               variant="inherit"
               color="primary"
             >
