7fe7ddeb7411275b2a6a9a181a6abccd1e18ead1
MatiasNasif <matiasezequielnasif@gmail.com>
2023-03-30 19:58:34 -0300

    feat:add service packagesByUser

--- api/src/packages/services/packages.service.ts
+++ api/src/packages/services/packages.service.ts
@@ -3,12 +3,16 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { PackageDocument } from '../entities/packages.entity';
 import { CreatePackageDto } from '../dtos/packages.dto';
+import { UserDocument } from 'src/users/entities/user.entity';
+import { NotFoundException } from '@nestjs/common/exceptions';
 
 @Injectable()
 export class PackagesService {
   constructor(
     @InjectModel('package')
     private readonly packageModel: Model<PackageDocument>,
+    @InjectModel('user')
+    private readonly userModel: Model<UserDocument>,
   ) {}
 
   createPackage(createPackageDto: CreatePackageDto): Promise<CreatePackageDto> {
@@ -26,8 +30,28 @@ export class PackagesService {
   }
 
   async getAllPackagesByUser(userId: string): Promise<CreatePackageDto[]> {
-    const packages = await this.packageModel.find({ user: userId });
-    return packages;
+    const userExists = await this.userModel.findById(userId);
+    if (!userExists) {
+      throw new NotFoundException('No existe el usuario en la base de datos');
+    }
+
+    const packagesByUser = await this.packageModel.find({ user: userId });
+    return packagesByUser;
+  }
+
+  async getAllPackagesPendingByUser(
+    userId: string,
+  ): Promise<CreatePackageDto[]> {
+    const userExists = await this.userModel.findById(userId);
+    if (!userExists) {
+      throw new NotFoundException('No existe el usuario en la base de datos');
+    }
+
+    const packagesPendingByUser = await this.packageModel.find({
+      deliveryStatus: 'Pendiente',
+      user: userId,
+    });
+    return packagesPendingByUser;
   }
 
   async deletePackage(packageId: string): Promise<CreatePackageDto> {
