da1015c559134501c155f47ec7c7acf5cbd9d889
MatiasNasif <matiasezequielnasif@gmail.com>
2023-04-04 19:22:23 -0300

    refactor: connection api view current distribution

--- client/src/pages/views/current-distribution.tsx
+++ client/src/pages/views/current-distribution.tsx
@@ -2,7 +2,6 @@ import styles from '../../styles/CurrentDistribution.module.css';
 import Image from 'next/image';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import maps from '../../assets/maps.png';
-import dummyData from '../../dummy-data/package-dummy.json';
 import {
   Box,
   Container,
@@ -13,56 +12,101 @@ import {
   Typography,
 } from '@mui/material';
 import ArrowApp from '@/commons/arrowApp';
-import Header from '/@/commons/header';
+import Header from '@/commons/header';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+interface User {
+  id: string;
+}
+
+interface Package {
+  address: string;
+  receiver: string;
+  weight: number;
+  deliveryDate: string;
+  quantity: number;
+  deliveryStatus: string;
+  user?: string;
+  _id?: string;
+}
 
 export default function CurrentDistribution() {
+  let user: User | null = null;
+  if (typeof window !== 'undefined') {
+    const userLocalStorage: string | null = localStorage.getItem('user');
+    user = userLocalStorage !== null ? JSON.parse(userLocalStorage) : null;
+  }
+
+  const userId: string | undefined = user?.id;
+  const API_URL: string = 'http://localhost:5000';
+
+  const [packagesByUser, setPackagesByUser] = useState<Package[]>([]);
+
+  useEffect(() => {
+    fetch(`${API_URL}/packages/${userId}/packagesByUser`)
+      .then((response) => response.json())
+      .then((packagesByUser: Package[]) => setPackagesByUser(packagesByUser))
+      .catch((error) => console.log(error));
+  }, [userId]);
+
   return (
     <Container maxWidth="xs" disableGutters={true}>
       <Header />
-      <ArrowApp />
+      <Link href={'/views/get-packages'}>
+        <ArrowApp />
+      </Link>
 
       <Box className={styles.container_accordion}>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography variant="h6" className={styles.container_accordion_title}>
-              Reparto en curso
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Box>
-              <Image src={maps} alt="maps" className={styles.container_accordion_maps} />
+        {packagesByUser?.map((packByUser: Package, i: number) => (
+          <Accordion key={i} className={styles.accordion_space}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography variant="h6" className={styles.container_accordion_title}>
+                {`Reparto ${packByUser.deliveryStatus}`}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Box>
+                <Image src={maps} alt="maps" className={styles.container_accordion_maps} />
+              </Box>
+              <Box>
+                <Typography className={styles.container_accordion_subtitle}>
+                  Destino:{' '}
+                  <span className={styles.container_accordion_subtitle_details}>
+                    {packByUser.address}
+                  </span>
+                </Typography>
+                <Typography className={styles.container_accordion_subtitle}>
+                  # del paquete:{' '}
+                  <span className={styles.container_accordion_subtitle_details}>
+                    {packByUser._id}
+                  </span>
+                </Typography>
+                <Typography className={styles.container_accordion_subtitle}>
+                  Recibe:{' '}
+                  <span className={styles.container_accordion_subtitle_details}>
+                    {packByUser.receiver}
+                  </span>
+                </Typography>
+              </Box>
+            </AccordionDetails>
+            <Box className={styles.container_button}>
+              {packByUser.deliveryStatus != 'Entregado' ? (
+                <Button className={styles.button} variant="contained">
+                  Finalizar
+                </Button>
+              ) : (
+                <Button className={styles.button} variant="contained" disabled={true}>
+                  Finalizado
+                </Button>
+              )}
             </Box>
-            <Box>
-              <Typography className={styles.container_accordion_subtitle}>
-                Destino:{' '}
-                <span className={styles.container_accordion_subtitle_details}>
-                  {dummyData[0].address}
-                </span>
-              </Typography>
-              <Typography className={styles.container_accordion_subtitle}>
-                # del paquete:{' '}
-                <span className={styles.container_accordion_subtitle_details}>
-                  {dummyData[0].id}
-                </span>
-              </Typography>
-              <Typography className={styles.container_accordion_subtitle}>
-                Recibe:{' '}
-                <span className={styles.container_accordion_subtitle_details}>
-                  {dummyData[0].receiver}
-                </span>
-              </Typography>
-            </Box>
-          </AccordionDetails>
-          <Box className={styles.container_button}>
-            <Button className={styles.button} variant="contained">
-              Finalizar
-            </Button>
-          </Box>
-        </Accordion>
+          </Accordion>
+        ))}
       </Box>
     </Container>
   );
