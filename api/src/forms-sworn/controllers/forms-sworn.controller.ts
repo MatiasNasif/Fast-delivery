b37ee97df18e63c210a7a13157f42fee934d4b2d
fabian <fabianlopez1988@gmail.com>
2023-05-04 17:42:56 -0300

    feat: add swagger documentation to api endpoints

--- api/src/forms-sworn/controllers/forms-sworn.controller.ts
+++ api/src/forms-sworn/controllers/forms-sworn.controller.ts
@@ -11,11 +11,28 @@ import {
 } from '@nestjs/common';
 import { FormSwornService } from '../services/form-sworn.service';
 import { CreateFormSwornDto, UpdateFormSwornDto } from '../dtos/form-sworn.dto';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiParam,
+  ApiBody,
+  ApiOkResponse,
+  ApiNotFoundResponse,
+} from '@nestjs/swagger';
 
+@ApiTags('form-sworn')
 @Controller('formsworn')
 export class FormSwornController {
   constructor(private readonly formSwornService: FormSwornService) {}
 
+  @ApiOperation({ summary: 'Crear formulario' })
+  @ApiBody({ type: CreateFormSwornDto })
+  @ApiResponse({
+    status: 201,
+    description: 'El formulario ha sido creado correctamente',
+  })
+  @ApiResponse({ status: 400, description: 'Petición inválida' })
   @Post('createforms')
   async create(@Body() createFormSwornDto: CreateFormSwornDto) {
     const formSworn = await this.formSwornService.createFormSworn(
@@ -24,11 +41,27 @@ export class FormSwornController {
     return formSworn;
   }
 
+  @ApiOperation({ summary: 'Devolver todos los formularios' })
+  @ApiResponse({
+    status: 201,
+    description: 'Muestra todos los formularios correctamente',
+  })
+  @ApiResponse({ status: 400, description: 'Petición inválida' })
   @Get()
   async getFormSworn(): Promise<CreateFormSwornDto[]> {
     return await this.formSwornService.getFormSworn();
   }
 
+  @ApiOperation({
+    summary: 'Devolver todos los formularios de un usuario por ID',
+  })
+  @ApiResponse({
+    status: 200,
+    description:
+      'Los formularios del usuario solicitado se muestran correctamente',
+  })
+  @ApiParam({ name: 'userId', type: 'string' })
+  @ApiResponse({ status: 404, description: 'Usuario no encontrado' })
   @Get('getAll')
   async getAllFormSwornByUser(
     @Query('userId') userId: string,
@@ -36,6 +69,16 @@ export class FormSwornController {
     return await this.formSwornService.getAllFormSwornByUser(userId);
   }
 
+  @ApiOperation({
+    summary: 'Devuelve un formulario en específico de un usuario por ID',
+  })
+  @ApiResponse({
+    status: 200,
+    description:
+      'El formulario del usuario solicitado se muestra correctamente',
+  })
+  @ApiParam({ name: 'userId', type: 'string' })
+  @ApiResponse({ status: 404, description: 'Usuario no encontrado' })
   @Get(':userId')
   async getFormSwornByUserId(@Param('userId') userId: string) {
     const formSwornByUserId = await this.formSwornService.getFormSwornByUserId(
@@ -47,6 +90,14 @@ export class FormSwornController {
     return formSwornByUserId;
   }
 
+  @ApiOperation({ summary: 'Actualizar un formulario por ID' })
+  @ApiParam({ name: 'id', type: 'string' })
+  @ApiBody({ type: UpdateFormSwornDto })
+  @ApiOkResponse({
+    description: 'El formulario ha sido actualizado correctamente',
+    type: UpdateFormSwornDto,
+  })
+  @ApiResponse({ status: 404, description: 'Formulario no encontrado' })
   @Put(':id')
   async updateFormSworn(
     @Param('id') id: string,
@@ -64,6 +115,18 @@ export class FormSwornController {
     return FormSwornUpdate;
   }
 
+  @ApiOperation({ summary: 'Eliminar un formulario' })
+  @ApiParam({
+    name: 'id',
+    type: 'string',
+    description: 'ID del formulario a eliminar',
+  })
+  @ApiOkResponse({
+    description: 'El formulario ha sido eliminado exitosamente.',
+  })
+  @ApiNotFoundResponse({
+    description: 'El formulario a eliminar no fue encontrado.',
+  })
   @Delete(':id')
   async deleteFormSworn(@Param('id') id: string) {
     const formSwornDelete = await this.formSwornService.deleteFormSworn(id);
--- api/src/packages/controllers/packages.controller.ts
+++ api/src/packages/controllers/packages.controller.ts
@@ -10,35 +10,82 @@ import {
 } from '@nestjs/common';
 import { CreatePackageDto, UpdatePackageDto } from '../dtos/packages.dto';
 import { PackagesService } from '../services/packages.service';
+import {
+  ApiTags,
+  ApiResponse,
+  ApiParam,
+  ApiBadRequestResponse,
+  ApiNotFoundResponse,
+  ApiOperation,
+  ApiBody,
+} from '@nestjs/swagger';
 
+@ApiTags('packages')
 @Controller('packages')
 export class PackagesController {
   constructor(private readonly packageService: PackagesService) {}
 
+  @ApiOperation({ summary: 'Crear paquete' })
+  @ApiResponse({
+    status: 201,
+    description: 'El paquete fue creado exitosamente.',
+    type: CreatePackageDto,
+  })
+  @ApiBadRequestResponse({ description: 'La data provista es inválida.' })
+  @ApiBody({ type: CreatePackageDto })
   @Post('create')
   createPackage(@Body() createPackageDto: CreatePackageDto) {
     const newPackage = this.packageService.createPackage(createPackageDto);
     return newPackage;
   }
 
+  @ApiOperation({ summary: 'Devolver todos los paquetes' })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de todos los paquetes.',
+    type: [CreatePackageDto],
+  })
   @Get()
   async getPackages() {
     const packages = await this.packageService.getPackages();
     return packages;
   }
 
+  @ApiOperation({ summary: 'Obtener todos los paquetes pendientes' })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de todos los paquetes pendientes en el sistema.',
+    type: [CreatePackageDto],
+  })
   @Get('/packagesPending')
   async getAllPackagesPending() {
     const packages = await this.packageService.getAllPackagesPending();
     return packages;
   }
 
+  @ApiOperation({
+    summary: 'Obtener todos los paquetes pendientes no asignados',
+  })
+  @ApiResponse({
+    status: 200,
+    description:
+      'Lista de todos los paquetes pendientes y no asignados en el sistema.',
+    type: [CreatePackageDto],
+  })
   @Get('/packagesPendingNotAssign')
   async getAllPackagesPendingNotAssign() {
     const packages = await this.packageService.getAllPackagesPendingNotAssign();
     return packages;
   }
 
+  @ApiOperation({ summary: 'Obtener paquete por ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'El paquete correspondiente al ID especificado.',
+    type: CreatePackageDto,
+  })
+  @ApiNotFoundResponse({ description: 'El paquete no existe.' })
+  @ApiParam({ name: 'packageId', description: 'ID del paquete' })
   @Get('/:packageId')
   async getPackage(@Param('packageId') packageId) {
     const packageid = await this.packageService.getPackage(packageId);
@@ -46,6 +93,12 @@ export class PackagesController {
     return packageid;
   }
 
+  @ApiOperation({ summary: 'Obtener todos los paquetes por usuario' })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de todos los paquetes correspondientes al usuario.',
+    type: [CreatePackageDto],
+  })
   @Get('/:userId/packagesByUser')
   async getAllPackagesByUser(@Param('userId') userId) {
     const packagesByUser = await this.packageService.getAllPackagesByUser(
@@ -54,6 +107,16 @@ export class PackagesController {
     return packagesByUser;
   }
 
+  @ApiOperation({
+    summary: 'Obtener todos los paquetes pendientes por usuario',
+  })
+  @ApiResponse({
+    status: 200,
+    description:
+      'Lista de todos los paquetes pendientes correspondientes al usuario.',
+    type: [CreatePackageDto],
+  })
+  @ApiParam({ name: 'userId', description: 'ID del usuario' })
   @Get('/:userId/packagesPendingByUser')
   async getAllPackagesPendingByUser(@Param('userId') userId) {
     const packagesPendingByUser =
@@ -61,6 +124,15 @@ export class PackagesController {
     return packagesPendingByUser;
   }
 
+  @ApiOperation({ summary: 'Obtener paquetes por fecha de entrega' })
+  @ApiResponse({
+    status: 200,
+    description:
+      'Lista de todos los paquetes correspondientes a la fecha de entrega especificada.',
+    type: [CreatePackageDto],
+  })
+  @ApiNotFoundResponse({ description: 'No se encontraron paquetes.' })
+  @ApiParam({ name: 'deliveryDate', description: 'fecha de entrega' })
   @Get(':deliveryDate/delivery-date')
   async findByDeliveryDate(@Param('deliveryDate') deliveryDateString: string) {
     const packages = await this.packageService.findByDeliveryDate(
@@ -74,6 +146,14 @@ export class PackagesController {
     return packages;
   }
 
+  @ApiOperation({ summary: 'Actualizar un paquete' })
+  @ApiResponse({
+    status: 200,
+    description: 'El paquete fue actualizado exitosamente.',
+    type: UpdatePackageDto,
+  })
+  @ApiNotFoundResponse({ description: 'El paquete no existe.' })
+  @ApiParam({ name: 'packageId', description: 'ID del paquete' })
   @Put('/:packageId')
   async packageId(
     @Param('packageId') packageId: string,
@@ -82,6 +162,13 @@ export class PackagesController {
     return this.packageService.updatePackage(packageId, updatePackage);
   }
 
+  @ApiOperation({ summary: 'Eliminar un paquete' })
+  @ApiResponse({
+    status: 200,
+    description: 'El paquete fue eliminado exitosamente.',
+  })
+  @ApiNotFoundResponse({ description: 'El paquete no existe.' })
+  @ApiParam({ name: 'packageId', description: 'ID del paquete' })
   @Delete('/:packageId')
   async deletePackage(@Param('packageId') packageId) {
     const deletePackage = await this.packageService.deletePackage(packageId);
